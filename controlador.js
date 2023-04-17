c191737f518862847b0c702435622c483dc33f43
“Monike” <monike.l@aluno.ifsc.edu.br>
2023-04-17 20:20:28 -0300

    Criação de controlador

---
+++ controlador.js
@@ -0,0 +1,69 @@
+var path = require('path');
+
+const controlador = {}
+let indice = 4
+var lista = [
+    {"id": 1,
+    "descricao": "Camiseta",
+    "preco": 29.99,
+    "cores": ["preto", "branco", "cinza"]},
+
+    {"id": 2,
+    "descricao": "Calça Jeans",
+    "preco": 79.99,
+    "cores": ["azul claro", "azul escuro"]},
+
+    {"id": 3,
+    "descricao": "Tênis Esportivo",
+    "preco": 149.99,
+    "cores": ["preto", "branco", "cinza", "azul"]},
+
+    {"id": 4,
+    "descricao": "Jaqueta de Couro",
+    "preco": 99.99,
+    "cores": ["preto", "marrom"]}
+]
+controlador.getAllPessoas = (req,res)=>{
+    res.status(200).send(lista)
+}
+
+controlador.getPessoaById = (req,res)=>{
+    item = lista.find(i => i.id == req.params.id)
+  
+    if(item){
+      res.status(200).send(item)
+    } else{
+      res.status(404).sendFile(path.resolve(__dirname+"/../views/notfound.html"))
+    }
+  }
+
+controlador.createPessoa = (req,res)=>{
+    const novaPessoa = req.body
+    novaPessoa.id = indice + 1
+    lista.push(novaPessoa)
+    res.status(200).redirect("/pessoas")
+}
+
+controlador.updatePessoa = (req,res)=>{
+    pessoaIndice = lista.findIndex(p => p.id == req.params.id)
+    console.log(pessoaIndice)
+    if(pessoaIndice >= 0){
+      const pessoaAtualizada = req.body;
+      lista[pessoaIndice] = pessoaAtualizada;
+      res.status(200).send("OK")
+    }else{
+      res.status(404).sendFile(path.resolve(__dirname+"/../views/notfound.html"))
+    }
+}
+
+controlador.deletePessoa = (req,res)=>{
+  const pessoaIndice = lista.findIndex(p => p.id == req.params.id);
+  if (pessoaIndice >= 0) {
+    lista.splice(pessoaIndice, 1);
+    res.status(200).send("OK")
+  }else{
+    res.status(404).sendFile(path.resolve(__dirname+"/../views/notfound.html"))
+  }
+}
+
+module.exports = controlador
--- rotas.js
+++ rotas.js
@@ -1,5 +1,17 @@
 const express = require("express")
-const routes = express.Router()
+const routes = require("../controlador")
 
+routes.get("/pessoas",controlador.getAllPessoas)
+routes.get("/pessoas/:id",controlador.getPessoaById)
+routes.post("/pessoas",controlador.createPessoa)
+routes.put("/pessoas/:id",controlador.updatePessoa)
+routes.delete("/pessoas/:id",controlador.deletePessoa)
+//routes.get("/",(req,res)=>{
+//    res.status(200).json(lista);
+//});    
+//
+//routes.get("/:id",(req,res)=>{
+//    res.status(200).json(lista[req.params.id-1]);
+//});
 
 module.exports = routes
